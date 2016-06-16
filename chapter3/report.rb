a8693acd87cf289bfa3c8148e7b5dbd8156bad44
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2016-06-16 19:01:59 +0900

    chapter3.1: add plain text format

--- chapter3/report.rb
+++ chapter3/report.rb
@@ -4,19 +4,34 @@ class Report
     @text = ['順調', '最高の調子']
   end
 
-  def output_report
-    puts '<html>'
-    puts ' <head>'
-    puts " <title>#{@title}</title>"
-    puts ' </head>'
-    puts ' <body>'
-    @text.each do |line|
-      puts " <p>#{line}</p>"
+  def output_report(format)
+    if format == :plain
+      puts "*** #{@title} ***"
+    elsif format == :html
+      puts '<html>'
+      puts ' <head>'
+      puts " <title>#{@title}</title>"
+      puts ' </head>'
+      puts ' <body>'
+    else
+      raise "Unknown format: #{format}"
     end
-    puts ' </body>'
-    puts '</html>'
+
+    @text.each do |line|
+      if format == :plain
+        puts line
+      else
+        puts " <p>#{line}</p>"
+      end
+    end
+
+    if format == :html
+      puts ' </body>'
+      puts '</html>'
+    end
   end
 end
 
 report = Report.new
-report.output_report
+report.output_report(:plain)
+report.output_report(:html)
