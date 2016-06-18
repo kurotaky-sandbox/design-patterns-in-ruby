22618eb338648056ddef75c909e830c25866cbbf
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2016-06-18 19:55:30 +0900

    more observable

--- chapter5/employee.rb
+++ chapter5/employee.rb
@@ -5,23 +5,48 @@ class Payroll
   end
 end
 
+class TaxMan
+  def update(changed_employee)
+    puts "#{changed_employee.name}に新しい税金の請求書を送ります！"
+  end
+end
+
 class Employee
   attr_reader :name
   attr_reader :title, :salary
 
-  def initialize(name, title, salary, payroll)
+  def initialize(name, title, salary)
     @name = name
     @title = title
     @salary = salary
-    @payroll = payroll
+    @observers = []
   end
 
   def salary=(new_salary)
     @salary = new_salary
-    @payroll.update(self)
+    notify_observers
+  end
+
+  def notify_observers
+    @observers.each do |observer|
+      observer.update(self)
+    end
+  end
+
+  def add_observer(observer)
+    @observers << observer
+  end
+
+  def delete_observer(observer)
+    @observers.delete(observer)
   end
 end
 
+fred = Employee.new('Fred', 'Crane Operator', 30000.0)
+
 payroll = Payroll.new
-fred = Employee.new('Fred', 'Crane Operator', 30000, payroll)
-fred.salary = 35000
+fred.add_observer payroll
+tax_man = TaxMan.new
+fred.add_observer tax_man
+
+fred.salary = 35000.0
