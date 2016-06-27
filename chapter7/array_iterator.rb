897093fd188056c676ce88aade325b341e190967
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2016-06-28 00:20:51 +0900

    iterator

---
+++ chapter7/array_iterator.rb
@@ -0,0 +1,33 @@
+class ArrayIterator
+  def initialize(array)
+    @array = array
+    @index = 0
+  end
+
+  def has_next?
+    @index < @array.length
+  end
+
+  def item
+    @array[@index]
+  end
+
+  def next_item
+    value = @array[@index]
+    @index += 1
+    value
+  end
+end
+
+array = ['red', 'green', 'blue']
+
+i = ArrayIterator.new(array)
+while i.has_next?
+  puts "item: #{i.next_item}"
+end
+
+i = ArrayIterator.new('abc')
+
+while i.has_next?
+  puts "item: #{i.next_item.chr}"
+end
